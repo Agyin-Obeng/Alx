9d41bfc2b1fb870e9758163b4c75a836422c39e0
Agyin-Obeng <agyinobeng60@gmail.com>
2025-11-29 10:02:38 +0000

    Added migrations

---
+++ week 12 new/api_project/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework import viewsets
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# NEW — CRUD ViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+  # restrict access to authenticated users only
+
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ('GET', 'HEAD', 'OPTIONS'):
+            return True
+        return request.user and request.user.is_staff
+
+
+
